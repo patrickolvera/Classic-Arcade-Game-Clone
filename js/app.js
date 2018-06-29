5bd6beb5ab3a48b4bd720b45c8ff1f6a1d0c1379
Patrick Olvera <patrickolvera@gmail.com>
2018-06-29 17:33:44 -0500

    Feat: Initialize player, enemy, and canvas background

--- js/app.js
+++ js/app.js
@@ -1,35 +1,50 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
+class Enemy {
+    // Constuct enemies with a given x & y position, and speed
+    constructor(x, y, speed = 1) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+    }
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+    }
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// The player we will be controlling
+class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.sprite = 'images/char-boy.png';
+    }
+    update(dt) {
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    handleInput() {
 
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+const allEnemies = [new Enemy(20, 60, 10)];
+const player = new Player(200, 400);
 
 
 // This listens for key presses and sends the keys to your
